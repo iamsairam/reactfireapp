673365c3b87c9c78152aa5403361962bbe2f1fb7
sairam <sairam@apporbits.com>
2022-01-26 22:24:47 +0530

    commit header tag

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Start the Game Now!!!
+       <h1>Start the Game Now!!!</h1>
       </header>
     </div>
   );
